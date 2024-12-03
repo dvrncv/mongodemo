269455089ac8680c24f4500e5168a99d9440ccbf
macostacos <ivanov.maxim0@yandex.ru>
2024-12-03 19:17:44 +0300

    test

--- src/main/java/com/example/demo/Dto/TourDto.java
+++ src/main/java/com/example/demo/Dto/TourDto.java
@@ -24,6 +24,7 @@ public class TourDto {
         this.price = price;
         this.duration = duration;
         this.features = features;
+        
         this.startDate = startDate;
         this.finishDate = finishDate;
         this.rating = rating;
